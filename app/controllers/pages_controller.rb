7c4e4552d800e0bea0e5f55465ce8999bdb07fb5
Hatemii <hatem.xhezairi96@gmail.com>
2022-08-12 16:17:26 +0200

    init

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+require "net/http"
+require "uri"
+require "json"
+
+class PagesController < ApplicationController
+  def index
+  end
+
+  def home
+  end
+
+  def signug
+  end
+
+  def login
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'pages/index'
+  get 'pages/home'
+  get 'pages/signug'
+  get 'pages/login'
+end
